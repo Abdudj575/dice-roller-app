fc17a13e8cc3ccea1a81e3a3dbf569805130d068
abdu djuraev <abdudj575@gmail.com>
2026-04-07 04:00:01 -0400

    dice-04: Make Die configurable with props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-green-700">
       <h1 className="text-4xl font-bold text-white">Dice Roller</h1>
-      <Die />
+      <Die value={4}/>
     </div>
   );
 }
