6aff1eb9580dd6a61aa1c753a10965574604c6ae
abdu djuraev <abdudj575@gmail.com>
2026-04-07 04:18:45 -0400

    dice-05: Add interactivity with useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import Die from "./components/Die";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-green-700">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-green-700">
       <h1 className="text-4xl font-bold text-white">Dice Roller</h1>
-      <Die value={4}/>
+
+      <div className="flex gap-6"> {/* 👀 Wrap dice in a flex container */}
+        <Die initialValue={3} />
+        <Die initialValue={5} /> {/* 👀 Add a second die */}
+      </div>
     </div>
   );
 }
